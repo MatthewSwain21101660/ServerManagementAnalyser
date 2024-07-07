60b4a9130c4fb3096265a192f5851832eebff8b0
Matthew <meswain8@gmail.com>
2024-07-07 21:26:46 +0100

    Updated ports

--- src/main/java/uk/ac/hope/mcse/segh/servermanagement/model/HardwareReading.java
+++ src/main/java/uk/ac/hope/mcse/segh/servermanagement/model/HardwareReading.java
@@ -2,7 +2,6 @@ package uk.ac.hope.mcse.segh.servermanagement.model;
 
 import jakarta.persistence.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import oshi.hardware.GlobalMemory;
 
 import java.math.BigInteger;
 
@@ -15,40 +14,23 @@ public class HardwareReading {
 
     private String dateTime;
 
-    private double cpu;
+    private String cpu;
 
-    private double ram;
-
-
-    public BigInteger get_id() {
-        return _id;
-    }
+    private String ram;
 
     public void set_id(BigInteger _id) {
         this._id = _id;
-    }
-
-    public String getDateTime() {
-        return dateTime;
     }
 
     public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
     }
 
-    public double getCpu() {
-        return cpu;
-    }
-
-    public void setCpu(double cpu) {
+    public void setCpu(String cpu) {
         this.cpu = cpu;
     }
 
-    public double getRam() {
-        return ram;
-    }
-
-    public void setRam(double ram) {
+    public void setRam(String ram) {
         this.ram = ram;
     }
 
